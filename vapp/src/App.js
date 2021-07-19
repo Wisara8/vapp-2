fc5f0077763c2f86bdcdc714249975d8b6313a95
wisara88 <srandall845@gmail.com>
2021-07-19 13:00:54 -0700

    tabs working

--- vapp/src/App.js
+++ vapp/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Tabs, Tab, AppBar } from '@material-ui/core';
+
 
 function App() {
+  const [value, setValue] = React.useState(0);
+  const handleTabs = (e, value) => {
+    console.log(value);
+    setValue(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Off Grid Customs
+      </h1>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleTabs}>
+          <Tab label="Item 1" />
+          <Tab label="Item 2" />
+          <Tab label="Item 3" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>Item 1 Detail</TabPanel>
+      <TabPanel value={value} index={1}>Item 2 Detail</TabPanel>
+      <TabPanel value={value} index={2}>Item 3 Detail</TabPanel>
+
     </div>
   );
 }
 
 export default App;
+
+function TabPanel(props) {
+  const { children, value, index } = props;
+  return (<div>
+    {
+      value === index && (
+        <h1>{children}</h1>
+      )
+    }
+  </div>)
+}
