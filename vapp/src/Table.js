235b4d4878231bed91bb65691b1be449da82963d
wisara88 <srandall845@gmail.com>
2021-07-26 23:20:17 -0700

    table

---
+++ vapp/src/Table.js
@@ -0,0 +1,96 @@
+const __waterHeaters = {
+  dwh1: {
+    model: "small water Heater",
+    capacity: "20l",
+    watts: "3000w",
+    voltage: "120",
+    height: "36in",
+    diameter: "12in",
+    weight: "70lbs",
+  },
+  dwh2: {
+    model: "medium water Heater",
+    capacity: "35l",
+    watts: "3800w",
+    voltage: "120",
+    height: "52in",
+    diameter: "18in",
+    weight: "120lbs"
+  },
+  dwh3: {
+    model: "large water Heater",
+    capacity: "50l",
+    watts: "4500w",
+    voltage: "120",
+    height: "63in",
+    diameter: "2in",
+    weight: "180lbs"
+  },
+};
+
+const TableHeader = ({ tableHeadData }) => {
+  return (
+    <div>
+      <thead>
+        <tr>
+          {
+            tableHeadData.map(header => (
+              <th>{header}</th>
+            )
+            )
+          }
+        </tr>
+      </thead>
+    </div>
+  );
+}
+
+const TableRows = ({ waterHeater }) => {
+  return (
+    <div>
+      <tbody>
+        {
+          waterHeater.map(heater => (
+            <tr>
+              <TableData heaterData={heater} />
+            </tr>)
+          )
+        }
+      </tbody>
+    </div>
+  )
+}
+
+const TableData = ({ heaterData }) => {
+  return (
+    heater.map(heaterData => (
+      <td>
+        {heaterData}
+      </td>
+    )))
+}
+
+const App = ({ waterHeater }) => {
+
+
+  const waterHeaterItem = waterHeater.dwh1;
+
+  const tableHeadData = [];
+
+  function getHeaders(waterHeaterItem) {
+    for (const item in waterHeaterItem) {
+      tableHeadData.push(item);
+    }
+  };
+
+  getHeaders(waterHeaterItem);
+
+  return (
+    <main>
+      <TableHeader tableHeadData={tableHeadData} />
+      <TableRows waterHeater={waterHeater} />
+    </main>
+  )
+};
+
+ReactDOM.render(<App waterHeater={__waterHeaters} />, document.getElementById('app'))
